42ad288033d4895ae6fa60990b758bd8fbc7d705
takanori-hayashi <mail@m.tknrh.me>
2020-10-02 14:17:46 +0900

    post処理追加

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
-const express = require('express');
 
+// server
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// line
+const line = require('@line/bot-sdk');
+const config = require('./config');
+const client = new line.Client(config);
+
+// route:index
 app.get('/', (req, res) => {
   res.send('Hello');
+});
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  
 });
 
 app.listen(port, () => {
