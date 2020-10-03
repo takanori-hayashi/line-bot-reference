4ffcb31dc52ba2147c3bc8aa64a3b6c8106a6f0c
takanori-hayashi <mail@m.tknrh.me>
2020-10-04 00:10:45 +0900

    ボタン追加

--- src/lib/templates/buttons.js
+++ src/lib/templates/buttons.js
@@ -1,25 +1,28 @@
 const buttos = {
   type: 'template',
   altText: 'ボタン',
-  title: 'ボタンサンプル',
-  text: 'ボタンをタップしてください',
-  actions: [
-    {
-      label: 'LINE アプリ',
-      type: 'uri',
-      uri: 'https://line.me/ja/',
-    },
-    {
-      label: 'あなたに返信する',
-      type: 'postback',
-      uri: 'こんにちは！ボタンを押してくれてありがとうございます！',
-    },
-    {
-      label: 'あなたにメッセージを送る',
-      type: 'message',
-      uri: 'こんばんわ！元気ですか？',
-    },
-  ],
+  template: {
+    type: 'buttos',
+    title: 'ボタンサンプル',
+    text: 'ボタンをタップしてください',
+    actions: [
+      {
+        label: 'LINE アプリ',
+        type: 'uri',
+        uri: 'https://line.me/ja/',
+      },
+      {
+        label: 'あなたに返信する',
+        type: 'postback',
+        uri: 'こんにちは！ボタンを押してくれてありがとうございます！',
+      },
+      {
+        label: 'あなたにメッセージを送る',
+        type: 'message',
+        uri: 'こんばんわ！元気ですか？',
+      },
+    ],
+  }
 };
 
 module.exports = buttos;
