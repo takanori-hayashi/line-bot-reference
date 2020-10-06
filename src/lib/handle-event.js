ae1de55869ba66cec15b763250b567108d43bf8b
takanori-hayashi <mail@m.tknrh.me>
2020-10-06 09:05:24 +0900

    アイコン表示名変更

--- src/lib/handle-event.js
+++ src/lib/handle-event.js
@@ -25,6 +25,8 @@ const handleText = (message, replyToken, resource) => {
       return client.replyMessage(replyToken, emojis);
     case 'スタンプ':
       return client.replyMessage(replyToken, sticker);
+    case 'アイコン':
+      return client.replyMessage(replyToken, iconDisplay)
     default:
       return replyText(replyToken, text)
   }
