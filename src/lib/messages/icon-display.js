97319e5570045e0eaae23c8ab2e88a81ff567b74
takanori-hayashi <mail@m.tknrh.me>
2020-10-06 09:12:41 +0900

    アイコン表示

---
+++ src/lib/messages/icon-display.js
@@ -0,0 +1,10 @@
+const iconDisplay = {
+  type: 'text',
+  text: 'こんにちは！',
+  sender: {
+      name: 'Cony',
+      iconUrl: 'https://source.unsplash.com/random/1000x1000'
+  }
+};
+
+module.exports = iconDisplay;
