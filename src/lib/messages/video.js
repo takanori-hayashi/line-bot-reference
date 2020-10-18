e83443edfb0cf0800e7af37af29c02f2a47aef89
takanori-hayashi <mail@m.tknrh.me>
2020-10-18 22:57:32 +0900

    調整

--- src/lib/messages/video.js
+++ src/lib/messages/video.js
@@ -2,5 +2,5 @@ module.exports = {
   type: 'video',
   originalContentUrl: `${process.env.APP_URL}/assets/video/video.mp4`,
   previewImageUrl: `${process.env.APP_URL}/assets/images/video-thumbnail.jpg`,
-  trackingId: `video001`,
+  trackingId: 'video001',
 };
